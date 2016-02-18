cfb57163a0c115679c0083604653cef2ccd728e9
jeremybini <jeremybini@gmail.com>
2016-02-18 18:24:36 -0500

    we are so safe

--- server/auth/auth.router.js
+++ server/auth/auth.router.js
@@ -12,31 +12,33 @@ var User = require('../api/users/user.model');
 
 router.post('/login', function (req, res, next) {
 	
-	// User.findOne(req.body.email)
-	// .then(function (user) {
-	// 	var salt = user.salt;
-	// 	var buffer = crypto.pbkdf2Sync(req.body.password, salt, iterations, bytes);
-	// 	var hash = buffer.toString('base64');
-	// 	if(hash!==user.password) throw HttpError(401);
-	// 	req.login(user, function () {
-	// 		res.json(user);
-	// 	});
-	// })
-	// .then(null, next);
+	if( req.body.email && req.body.password ) {
+		
+		User.findOne({ email: req.body.email })
+		.select('+salt +password')
+		.then(function (user) {
+			var salt = user.salt;
+			var hash = crypto.pbkdf2Sync(req.body.password, salt, iterations, bytes).toString('base64');
 
-	User.findOne(req.body).exec()
-	.then(function (user) {
-		if (!user) throw HttpError(401);
-		req.login(user, function () {
-			res.json(user);
-		});
-	})
-	.then(null, next);
+			if(hash!==user.password) throw HttpError(401);
+			
+			user = user.toObject();
+			delete user.salt;
+			delete user.password;
+
+			req.login(user, function () {
+				res.json(user);
+			});
+		})
+		.then(null, next);
+	
+	}
+
 });
 
 router.post('/signup', function (req, res, next) {
 	
-	var salt = crypto.randomBytes(16);
+	var salt = crypto.randomBytes(16).toString('base64');
 	var buffer = crypto.pbkdf2Sync(req.body.password, salt, iterations, bytes);
 	var hash = buffer.toString('base64');
 	
@@ -46,6 +48,7 @@ router.post('/signup', function (req, res, next) {
 
 	User.create(req.body)
 	.then(function (user) {
+		console.log('user', user);
 		req.login(user, function () {
 			res.status(201).json(user);
 		});
